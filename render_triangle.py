6bc74320786b5ac7b92aed404730f1489fa350f5
Stylianos Vassiliou <63289585+svassi04@users.noreply.github.com>
2025-03-20 14:44:00 +0200

    Add files via upload

--- render_triangle.py
+++ render_triangle.py
@@ -15,31 +15,29 @@ def draw_triangle(image, triangle, triangle_color):
     max_y = min(max(v0[1], v1[1], v2[1]), image.height - 1)
 
     # Compute edge properties
-    a0, b0, c0 = v1[1] - v2[1], v2[0] - v1[0], v1[0] * v2[1] - v2[0] * v1[1]
-    a1, b1, c1 = v2[1] - v0[1], v0[0] - v2[0], v2[0] * v0[1] - v0[0] * v2[1]
-    a2, b2, c2 = v0[1] - v1[1], v1[0] - v0[0], v0[0] * v1[1] - v1[0] * v0[1]
+    # a0, b0, c0 = v1[1] - v2[1], v2[0] - v1[0], v1[0] * v2[1] - v2[0] * v1[1]
+    # a1, b1, c1 = v2[1] - v0[1], v0[0] - v2[0], v2[0] * v0[1] - v0[0] * v2[1]
+    # a2, b2, c2 = v0[1] - v1[1], v1[0] - v0[0], v0[0] * v1[1] - v1[0] * v0[1]
+
+    a0, b0, c0 = v2[1] - v1[1], v1[0] - v2[0], v2[0] * v1[1] - v1[0] * v2[1]
+    a1, b1, c1 = v0[1] - v2[1], v2[0] - v0[0], v0[0] * v2[1] - v2[0] * v0[1]
+    a2, b2, c2 = v1[1] - v0[1], v0[0] - v1[0], v1[0] * v0[1] - v0[0] * v1[1]
 
 
     # Iterate over each pixel
-    for y in range(min_y, max_y + 1):
-        # Evaluate edge equations 
-        E0 = a0 * min_x + b0 * y + c0
-        E1 = a1 * min_x + b1 * y + c1
-        E2 = a2 * min_x + b2 * y + c2
-        # print (E0, E1, E2)
-        
+    for y in range(min_y, max_y + 1):        
         for x in range(min_x, max_x + 1):
+            # EvCalculatealuate edge equations 
+            E0 = a0 * x + b0 * y + c0
+            E1 = a1 * x + b1 * y + c1
+            E2 = a2 * x + b2 * y + c2
+            # print (E0, E1, E2)
             # Check if the point is left or right of all edges
-            if E0 >= 0 and E1 >= 0 and E2 >= 0 or E0 <= 0 and E1 <= 0 and E2 <= 0:
+            if E0 >= 0 and E1 >= 0 and E2 >= 0:
                 # Sets a pixel on the image if within bounds
                 if 0 <= x < image.width and 0 <= y < image.height:
                     image.putpixel((x, y), triangle_color)
             
-            # Increment edge equations for the next pixel
-            E0 += a0
-            E1 += a1
-            E2 += a2
-
 
 
 def main():
@@ -56,6 +54,7 @@ def main():
 
     # Save or show the image
     image.show()  # Opens the image viewer
+    image.save("red_triangle.png", "PNG")
 
 
 main()
